bf44b3b765d9fb1d3b1938be92261669fb383a62
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-09-20 23:19:47 +0700

    I give up

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const map = require('lib0/dist/map.cjs')
 
+const http = require('http')
 const WebSocketServer = require('rpc-websockets').Server
-const {ChangeSet, Text} = "@codemirror/state"
+const {ChangeSet, Text} = require("@codemirror/state")
 
 class Doc {
   constructor(docName) {
     this.docName = docName
     // The updates received so far (updates.length gives the current version)
     this.updates = []
-    this.doc = Text.of([`Start document-${docName}`])
+    this.doc = Text.of([""])
   }
 }
 
@@ -20,15 +21,37 @@ const getDoc = (docname) => map.setIfUndefined(docs, docname, () => {
   return doc
 })
 
-const server = new WebSocketServer({
-  port: 4443,
-  host: 'localhost'
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('okay')
+})
+
+const wss = new WebSocketServer({
+  noServer: true
+})
+
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = ws => {
+    wss.wss.emit('connection', ws, request)
+  }
+  wss.wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+wss.event("connection", e => {
+  console.log("OK", e)
 })
 
 // data = {docName, version, updates}
-server.register("pushUpdates", (data) => {
+wss.register("pushUpdates", (data) => {
+  console.log("Here")
   const doc = getDoc(data.docName)
   if (data.version !== doc.updates.length) {
+    wss.emit('newUpdates')
     return false;
   } else {
     for (let update of data.updates) {
@@ -38,19 +61,26 @@ server.register("pushUpdates", (data) => {
       doc.updates.push({changes, clientID: update.clientID})
       doc.doc = changes.apply(doc.doc)
     }
-    server.emit('newUpdates')
+    wss.emit('newUpdates')
     return true;
   }
 })
 
-server.register("pullUpdates", (data) => {
+wss.register("pullUpdates", (data) => {
   const doc = getDoc(data.docName)
   if (data.version < doc.updates.length) {
     return doc.updates.slice(data.version)
   }
 })
 
-server.register("getDocument", (data) => {
+wss.register("getDocument", (data) => {
   const doc = getDoc(data.docName)
   return {version: doc.updates.length, doc: doc.toString()}
+})
+
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 1234
+
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`)
 })
