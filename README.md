6d117762aa95c0ab288d58e944718558f170c1ed
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-09-22 13:35:54 +0700

    Use namespace

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 ```js
 {
-  type: "spawn"
+  type: "shell.spawn"
   clientID: clientID
   shellID: shellID
 }
 {
-  type: "info"
+  type: "shell.info"
   shellID: shellID
   message: message
+}
+{
+  type: "shell.request"
+  fromID: clientID
+  toID: clientID
+  message: message
+}
+{
+  type: "shell.compile"
+  toID: clientID
+  message: message
+}
+{
+  type: "shell.keystroke"
+  toID: clientID
+  keystroke: keystroke
 }
 ```
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const WebSocketServer = require('rpc-websockets').Server
 const {ChangeSet, Text} = require("@codemirror/state")
 
 class Doc {
-  constructor(docName) {
+  constructor(docName, namespace) {
     this.docName = docName
     // The updates received so far (updates.length gives the current version)
     this.updates = []
-    this.connections = new Set()
+    this.namespace = namespace
     this.shellUpdates = []
     this.doc = Text.of([""])
   }
@@ -19,71 +19,73 @@ const docs = new Map()
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 4443
 
-const getDoc = (docname) => map.setIfUndefined(docs, docname, () => {
-  const doc = new Doc(docname)
-  docs.set(docname, doc)
-  return doc
-})
-
 const wss = new WebSocketServer({
   host: host,
   port: port
 })
 
-wss.event('newUpdates')
+const getDoc = (docname) => map.setIfUndefined(docs, docname, () => {
+  const namespace = '/' + docname;
+  const doc = new Doc(docname, wss.namespaces[namespace])
+  wss.event('newUpdates', namespace)
+  wss.event('newPeers', namespace)
+  wss.register("getPeers", () => {
+    return doc.namespace.clients.keys()
+  }, namespace)
+
+  wss.register("pushUpdates", (data) => {
+    if (data.version !== doc.updates.length) {
+      doc.namespace.emit("newUpdates")
+      return false;
+    } else {
+      for (let update of data.updates) {
+        // Convert the JSON representation to an actual ChangeSet
+        // instance
+        let changes = ChangeSet.fromJSON(update.changes)
+        doc.updates.push({changes, clientID: update.clientID})
+        doc.doc = changes.apply(doc.doc)
+      }
+      doc.namespace.emit("newUpdates")
+      return true;
+    }
+  }, namespace)
+
+  wss.register("pushShellUpdates", (data) => {
+    const doc = getDoc(data.docName)
+    if (data.shellVersion !== doc.shellUpdates.length) {
+      doc.namespace.emit("newUpdates")
+      return false;
+    } else {
+      Array.prototype.push.apply(doc.shellUpdates, data.shellUpdates)
+      doc.namespace.emit("newUpdates")
+      return true;
+    }
+  }, namespace)
+
+  wss.register("pullUpdates", (data) => {
+    console.log(data.version, doc.updates.length)
+    let ret = {
+      updates: [],
+      shellUpdates: []
+    }
+    if (data.version < doc.updates.length) {
+      ret.updates = doc.updates.slice(data.version);
+    }
+    if (data.shellVersion < doc.shellUpdates.length) {
+      ret.shellUpdates = doc.shellUpdates.slice(data.shellVersion)
+    }
+    return ret;
+  }, namespace)
+
+  return doc
+})
 
 wss.on("listening", () => {
   console.log(`listening at ${wss.wss.options.host}:${wss.wss.options.port}`)
 })
 
-// data = {docName, version, updates}
-wss.register("pushUpdates", (data) => {
-  const doc = getDoc(data.docName)
-  if (data.version !== doc.updates.length) {
-    wss.emit("newUpdates")
-    return false;
-  } else {
-    for (let update of data.updates) {
-      // Convert the JSON representation to an actual ChangeSet
-      // instance
-      let changes = ChangeSet.fromJSON(update.changes)
-      doc.updates.push({changes, clientID: update.clientID})
-      doc.doc = changes.apply(doc.doc)
-    }
-    wss.emit("newUpdates")
-    return true;
-  }
-})
-
-wss.register("pushShellUpdates", (data) => {
-  const doc = getDoc(data.docName)
-  if (data.shellVersion !== doc.shellUpdates.length) {
-    wss.emit("newUpdates")
-    return false;
-  } else {
-    Array.prototype.push.apply(doc.shellUpdates, data.shellUpdates)
-    wss.emit("newUpdates")
-    return true;
-  }
-})
-
-wss.register("pullUpdates", (data) => {
-  const doc = getDoc(data.docName)
-  console.log(data.version, doc.updates.length)
-  let ret = {
-    updates: [],
-    shellUpdates: []
-  }
-  if (data.version < doc.updates.length) {
-    ret.updates = doc.updates.slice(data.version);
-  }
-  if (data.shellVersion < doc.shellUpdates.length) {
-    ret.shellUpdates = doc.shellUpdates.slice(data.shellVersion)
-  }
-  return ret;
-})
-
-wss.register("getDocument", (data) => {
-  const doc = getDoc(data.docName)
-  return {version: doc.updates.length, doc: doc.toString()}
+wss.on("connection", (ws, msg) => {
+  // console.log(ws)
+  const docName = msg.url.slice(1)
+  getDoc(docName)
 })
